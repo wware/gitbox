0fdb5393f79afedd4a6dd8bb71714709266dcbba
Will Ware <wware@alum.mit.edu>
2016-01-21 23:08:12 +0000

    Fix script name, and verbiage.

--- scripts/gitbox.sh
+++ scripts/gitbox.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          rpi-shutdown
+# Provides:          gitbox
 # Required-Start:    $syslog
 # Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Shutdown RPi gracefully when wall wart is yanked.
+# Short-Description: A box full of lovely little git repositories.
 ### END INIT INFO
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin
 
 . /lib/lsb/init-functions
-SCRIPT=/usr/sbin/shutdown.py
-PIDFILE=/var/run/shutdown.pid
+SCRIPT=/usr/sbin/gitbox.py
+PIDFILE=/var/run/gitbox.pid
 
 start() {
-    log_daemon_msg "Starting graceful shutdown service"
+    log_daemon_msg "Starting Gitbox service"
     (cd /opt/git; git instaweb --httpd=webrick --port=80) &
     $SCRIPT &
     pid=$!
@@ -29,7 +29,7 @@ start() {
 }
 
 stop() {
-    log_daemon_msg "Stopping graceful shutdown service"
+    log_daemon_msg "Stopping Gitbox service"
     if [ -f $PIDFILE ]; then
         kill -9 `cat $PIDFILE`
         rm -f $PIDFILE
